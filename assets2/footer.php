ba237c3e93c39e3db52c0db2247d216703462c3a
muttoharohh <45171076+muttoharohh@users.noreply.github.com>
2018-11-19 23:16:30 +0700

    Add files via upload

---
+++ assets2/footer.php
@@ -0,0 +1,93 @@
+<table width="100%" style="margin-bottom:25px;">
+  <tr>
+    <td width="6%" height="62">&nbsp;</td>
+    <td width="62%">&nbsp;</td>
+    <td width="4%">&nbsp;</td>
+    <td width="4%">&nbsp;</td>
+    <td width="4%">&nbsp;</td>
+    <td width="4%">&nbsp;</td>
+    <td width="4%">&nbsp;</td>
+    <td width="4%">&nbsp;</td>
+    <td width="4%">&nbsp;</td>
+    <td width="4%">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>PABW.GO.ID</td>
+    <td rowspan="6"><img src="image/fb-icon.png" width="182" height="91" /></td>
+    <td rowspan="6"><img src="image/twitter.png" width="93" height="93" /></td>
+    <td rowspan="6"><img src="image/instagram_PNG3.png" width="76" height="96" /></td>
+    <td rowspan="6">&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>Jl.Terusan Riya Cudu</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>Way Huwi, Lampung Selatan </td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="21">&nbsp;</td>
+    <td>Telp : (+6221) XXX XXXX </td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>Faks : (+6221) XXX XXXX</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>Surel : tubes@pabw.go.id </td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="64">&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="10"><div align="center">&copy; 2018 - 2019 Tugas Besar Pengembangan Aplikasi Berbasis Web Kelompok 4 </div></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
---
+++ assets2/navigasi.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<style type="text/css">
+ ul{
+	position: absolute;
+	margin: 0px;
+	padding: 0px;
+	list-style: none;
+	margin-left:20px;
+	top: 187px;
+	left: 51px;
+}
+ul li{
+    float: left;
+    width: 275px;
+    height: 60px;
+    background-color: black;
+    opacity: .6;
+    line-height: 60px;
+    text-align: center;
+    font-size: 15px;
+    margin-right: 10px;
+    margin-bottom: 20px;
+    margin-left: 15px;
+    margin-block-end: 40px;
+    text-align: center;
+	
+    
+}
+ul li a{
+    text-decoration: none;
+    color: white;
+    display: black;
+	float:left; 
+	width:275px; 
+	display:block; 
+	text-align: center; 
+	color:#FFF; 
+	text-decoration:none; 
+	text-transform:uppercase;
+}
+ul li a:hover{
+    background-color: rgb(175, 10, 37);
+	background-color: #669999; 
+	display:block;
+}
+</style>
+
+</head>
+
+<body>
+<ul>
+                <p align="center">
+               <li><a href="http://localhost/ciblog/application/views/pages/home">MENGENAL SKYPEA</a></li>
+               <li><a href="">TENTANG</a></li>
+               <li><a href="">KONTAK DAN INFORMASI</a></li>
+               <li><a href="">LAYANAN PUBLIK</a></li>
+               <li style="margin-left:315px;"><a href="">INFO DAERAH</a></li>
+               <li><a href="">POTENSI DAERAH</a></li>
+
+            </p>
+    </ul>
+
+</body>
+</html>
