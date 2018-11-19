7e63640f3e9bf161ea64780268d276b2b67a3252
hamzah14116124 <39445215+hamzah14116124@users.noreply.github.com>
2018-11-19 21:34:09 +0700

    Add files via upload

---
+++ application/views/users/login.php
@@ -0,0 +1,26 @@
+<?php echo form_open('users/login'); ?>
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			<h1 class="text-center"><?php echo $title; ?></h1>
+			<div class="form-group">
+				<input type="text" name="username" class="form-control" placeholder="Enter Username" required autofocus>
+			</div>
+			<div class="form-group">
+				<input type="password" name="password" class="form-control" placeholder="Enter Password" required autofocus>
+			</div>
+			<button type="submit" class="btn btn-primary btn-block">Login</button>
+		</div>
+	</div>
+<?php echo form_close(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title class="ab"><h1>Login Administrator</h1></title>
+   <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/background/style.css">
+</head>
+
+<body>
+
+</body>
+</html>
