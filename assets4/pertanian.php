f6d94417374e3068501a58bf32d04c97dd904bef
IndriWulanDari <38092286+IndriWulanDari@users.noreply.github.com>
2018-11-20 14:18:24 +0700

    Add files via upload

---
+++ assets4/pertanian.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Potensi Daerah</title>
+	<link rel="stylesheet" href="http://localhost/ciblog/assets4/style.css">
+</head>
+<body>
+	
+	<div class="bg_content">
+		<div class="logo1">
+		<h1><img src="http://localhost/ciblog/assets4/images/logo.png"></h1>
+		</div>
+		<h2 align="center">Pertanian Skypea</h2>
+		<p align="center">Tahun 2xxx</p></br>
+		<img src="http://localhost/ciblog/assets4/images/pertanian.jpg">
+		<h3>Potensi Di Bidang Pertanian</h2></br>
+		<p>Pertanian merupakan sektor terbesar penyumbang perekonomian di Daerah Skypea, Pertanian di sini mencakup pertanian tanaman bahan makanan, tanaman obat dan hias, perkebunan, kehutanan, peternakan. Produksi Tanaman Padi tahun 2012 mengalami kenaikan dibanding tahun 2011, hal ini dipengaruhi oleh luas panen, nilai produktivitas tanaman padi rata-rata mencapai 5,56 ton/ha. Produksi Tanaman Padi ladang pada tahun 2012 mengalami kenaikan yaitu mencapai 20 persen dari tahun sebelumnya dengan wilayah Kecamatan Kelumbayan Barat merupakan daerah produksi padi ladang terbesar dengan capaian produksi padi ladang sebesar 1.696 (18.80 %) dari total produksi se-Daerah Skypea. </p>
+
+		<p>Dari keseluruhan luas daratan di Daerah Skypea, luas areal yang digunakan dan disesuaikan untuk pertanian meliputi 20.643 Ha lahan sawah, serta 132.391 Ha lahan pertanian bukan sawah, dengan berbagai jenis prasarana irigasi/pengairan yang mendukungnya. Berbagai jenis pengairan yang digunakan bagi sektor pertanian di Kabupaten Tanggamus, dengan menggunakan irigasi teknis seluas 5.233 Ha, irigasi setengah teknis seluas 7.854 Ha, irigasi sederhana seluas 2.748 Ha, irigasi desa/non PU seluas 4.009 Ha dan tadah hujan seluas 799 Ha.</p>
+
+		<p>Penggunaan lahan bukan sawah di Daerah Skypea meliputi hutan dan perkebunan. Luas areal perkebunan di Daerah Skypea adalah 91.620,64 Ha yang meliputi perkebunan rakyat seluas 88.343,14 Ha, perkebunan swasta seluas 1.070 Ha dan perkebunan Negara seluas 2.207,5 Ha. Produksi Tanaman buah-buahan terbesar adalah durian sebesar  66.365 ton, disusul kemudian salak (23.170 ton) sehingga tidak salah apabila Daerah Skypea merupakan salah satu sentra buah durian.</p>
+	</div>
+	
+		
+	
+</body>
+</html>
