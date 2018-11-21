79fb3dd55602e135734970a66cbcfc5ccc96ffb8
IndriWulanDari <38092286+IndriWulanDari@users.noreply.github.com>
2018-11-21 21:48:50 +0700

    Update potensi.php

--- application/views/pages/potensi.php
+++ application/views/pages/potensi.php
@@ -17,7 +17,7 @@
 			<div>
 				<nav id="nav">
 					<ul>
-						<li class="active"><a href="<?php echo base_url(); ?>potensi">Pariwisata</a></li>
+						<li class="active"><a href="<?php echo base_url(); ?>pariwisata">Pariwisata</a></li>
 						<li ><a href="<?php echo base_url(); ?>peternakan">Peternakan</a></li>
 						<li ><a href="<?php echo base_url(); ?>pertanian">Pertanian</a></li>
 					</ul>
@@ -25,18 +25,6 @@
 			</div>
 		</div>
 	</div>
-	<div id="content" class="8u skel-cell-important">
-						<section>
-							<header>
-								<h2>Pariwisata Daerah Skypea</h2>
-								<span class="byline">Potensi Di Bidang Pariwisata</span>
-							</header>
-							<a href="#" class="image full"><img src="http://localhost/ciblog/assets4/images/pariwisata2.png" alt="" /></a>
-							<a href="#" class="image full"><img src="http://localhost/ciblog/assets4/images/pariwisata1.png" alt="" /></a>
-							<br><br>
-							<p>Bidang pariwisata <strong>Daerah Skypea</strong>, merupakan salah satu sektor ekonomi potensial yang dimiliki Daerah Skypea untuk dikembangkan sebagai sumber penghasilan guna meningkatkan Pendapatan Asli Daerah (PAD). Dengan membaiknya kondisi perekonomian serta jaminan keamanan akan memberikan dampak positif terhadap peningkatan wisatawan di wilayah Daerah Skypea. &ndash; Keindahan alam dan keanekaragaman adat dan budaya merupakan modal dasar yang dapat dikembangkan menjadi produk wisata menarik bagi wisatawan.
-							<p>Hal ini lah yang merupakan tantangan sekaligus peluang bagi para investor untuk dapat memanfaatkan potensi pariwisata yang ada di Daerah Skype. Dengan pengelolaan yang baik dan optimal, ditambah dengan pembangunan sarana dan prasarana di sekitar objek wisata, akan menambah daya tarik bagi para wisatawan </p>
-						</section>
-					</div>
+	
 </body>
 </html>
